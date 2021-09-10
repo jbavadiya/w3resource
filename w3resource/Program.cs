7b1f2eca461daf43d36a91138d278c987fe17b02
jbavadiya <jignesha.bavadiya@gmail.com>
2021-09-10 19:02:34 -0400

    Add project files.

---
+++ w3resource/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w3resource
+{
+   public class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string name;
+            Console.WriteLine("please enter your name");
+            name = Console.ReadLine();
+            Console.WriteLine($"hello:{name}");
+            Console.ReadLine();
+        }
+    }
+}
